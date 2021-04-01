27a3dcb4c67c2327f9d870b6765de32e32ce8af9
Roman Turner <rn.turner@live.com>
2021-04-01 15:13:32 -0700

    assignment complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { directors } from '../data';
 
+const showMovies = (d, index) => {
+  return (
+    <div key={index}>
+      <h3>Name: {d.name}</h3>
+      <p>Movies:</p>
+      <ul>
+        {d.movies.map((m, index) => (
+          <li key={index}>{m}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((d, index) => showMovies(d, index))}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
+
+const showFilms = (m, index) => {
+  return (
+    <div key={index}>
+      <h3>Title: {m.title}</h3>
+      <p>Time: {m.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {m.genres.map((g, index) => (
+          <li key={index}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((m, index) => {
+        return showFilms(m, index);
+      })}
     </div>
   );
 };
